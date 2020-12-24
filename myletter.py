f0cd90791508442a83e38e30adcd1ebf45c658f4
Prashant Dorugade <pdorugade@gmail.com>
2020-12-24 20:58:26 +0100

    commit my letter file

---
+++ myletter.py
@@ -0,0 +1,16 @@
+space = " "
+spaceCount = 50
+finalSpacing = spaceCount * space
+
+print("%s 12 Butts Wynd"% finalSpacing)
+print("%s Twinkle bottom Heath"% finalSpacing)
+print("%s Weat Snoring"% finalSpacing)
+print("")
+print("")
+print("Dear Sir")
+print("")
+print("I wish to report that tiles are missing from the outside toilet roof.")
+print("I think it was bad wind the other night that blew them away.")
+print("")
+print("Regards")
+print("Malcolm Dithering")
